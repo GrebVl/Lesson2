0e95ffa5914fc92ca18c5282beb0a8777f6c5a86
vladimir <chertonoivskiy@ucloud.com>
2022-07-16 09:23:17 +0600

    first commit task10

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿/*
+программа принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите число:");
+int firstNum = int.Parse(Console.ReadLine());
+
+int secondNum = firstNum / 100;
+int thirdNum = firstNum % 10;
+
+if (firstNum > 99 && firstNum < 1000 && firstNum < -99 && firstNum > -1000)
+{
+    Console.WriteLine((firstNum % 100-thirdNum)/10);
+}
+else
+{
+    Console.WriteLine("Число не является трех значным");
+}
