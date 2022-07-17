21bbb0e2ed0bfb794fe0b49d9b097116b1bc7f0f
vladimir <chertonoivskiy@ucloud.com>
2022-07-17 10:00:45 +0600

    second task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -11,20 +11,39 @@
 Console.WriteLine("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 
-
-if (100 < num && num < 1000)
+if (100 > num && num > -99)
+{
+    Console.WriteLine(num + " третьей цыфры не имеется");
+}
+if (99 < num && num < 1000)
 {
     Console.WriteLine(num % 10);
 }
-if (1000 < num && num < 10000)
+if (-1000 < num && num < -99)
+{
+    Console.WriteLine(num % 10);
+}
+if (999 < num && num < 10000)
 {
     Console.WriteLine((num % 100 - num % 10) / 10);
 }
-if (10000 < num && num < 100000)
+if (-1000 < num && num < -999)
+{
+    Console.WriteLine((num % 100 - num % 10) / 10);
+}
+if (9999 < num && num < 100000)
 {
     Console.WriteLine((num % 1000 - num % 100) / 100);
 }
-else
+if (-100000 < num && num < -9999)
 {
-    Console.WriteLine(num + " третьей цыфры не имеется");
+    Console.WriteLine((num % 1000 - num % 100) / 100);
+}
+if (99999 < num && num < 1000000)
+{
+    Console.WriteLine((num % 10000 - num % 1000) / 1000);
+}
+if (-10000000 < num && num < -99999)
+{
+    Console.WriteLine((num % 100000 - num % 10000) / 10000);
 }
