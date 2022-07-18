c056bb46a8cd269491759a2b487fb08bbff2de41
vladimir <chertonoivskiy@ucloud.com>
2022-07-18 08:37:20 +0600

    first commit taskDop

---
+++ taskDop/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+программ принимает на вход число N и выводит 
+все простые числа в промежутке 
+от 0 до N (включительно).
+
+*/
+
+
+
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+int index = 0;
+
+while (index <= num)
+{
+    if (index % 2 > 0 && index > 1)
+    {
+        Console.Write(index+" ");
+    }
+    if (index == 2)
+    {
+        Console.Write(index+" ");
+    }
+    index++;
+}
+Console.WriteLine(" ");
+
+
