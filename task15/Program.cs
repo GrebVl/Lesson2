d19477c794f83df5f31785b524d9018523695f45
vladimir <chertonoivskiy@ucloud.com>
2022-07-18 08:13:04 +0600

    second commit task13

--- task15/Program.cs
+++ task15/Program.cs
@@ -9,11 +9,16 @@
 Console.WriteLine("Введите число, обозначающую день недели: ");
 int num = int.Parse(Console.ReadLine());
 
+
 if (5 >= num && num >= 1)
 {
-    Console.WriteLine(num + " No");
+    Console.WriteLine(num + " Нет");
 }
 if (7 >= num && num >= 6)
 {
-    Console.WriteLine(num + " Yes");
+    Console.WriteLine(num + " Да");
+}
+else
+{
+    Console.WriteLine("Введите число в диапазоне от 1 до 7");
 }
